25f2da20522b0f5a1ead3ac6a0b226688de070ec
kunalhub <kunalraval73@gmail.com>
2019-07-31 17:29:28 +0100

    PORSONALDATA

---
+++ hellojava/src/PersonalData.java
@@ -0,0 +1,20 @@
+public class PersonalData {
+
+     public static void main (String arg [] ) {
+
+String name = "kunal Raval";
+int age = 25;
+String address = "94 ashbourn road, london, HA0 2NJ";
+long mobile = 7858221508L;
+double salary = 2100.89;
+
+System.out.println(name);
+System.out.println(age);
+System.out.println(address);
+System.out.println(mobile);
+System.out.println(salary);
+
+
+
+}
+}
